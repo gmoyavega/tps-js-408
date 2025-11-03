dbdf6a33a557786b32da171c28f69114d4d2e76b
Milu <mloeda@hotmail.com>
2025-11-03 18:57:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
+const form = document.getElementById('contactForm');
 const btn = document.getElementById('button');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    btn.disabled = true;
+    btn.value = 'Sending...';
+
+    
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbxCp8WdJzUx13oi6-OI6TJNfRsRos3_e2xztL-zho4oSS6bJ-rw1agndE87dJdAxoc6cA/exec';
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            
+            alert('Message sent! The data should appear in your Google Sheet shortly.');
+            form.reset();
+            btn.disabled = false;
+            btn.value = 'Enviar Mensaje';
+        })
+        .catch(error => {
+            console.error('Error!', error.message);
+            alert('An error occurred while sending the message. Please check the console for details and try again.');
+            btn.disabled = false;
+            btn.value = 'Enviar Mensaje';
+      
 
 document.getElementById("contactForm")
     .addEventListener('submit', function(event) {
@@ -17,4 +41,5 @@ document.getElementById("contactForm")
     btn.value = 'Send Email';
         alert(JSON.stringify(err));
     });
+
 });
